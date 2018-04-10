28024bb1e90f9c77329829e91da7121309ef8da1
Sakurai Shun <sakushun@SakurainoMacBook-Pro.local>
2018-04-10 10:36:50 +0900

    Create-registarion-page

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ group :development, :test do
 end
 
 gem 'devise'
+
+gem 'enum_help'
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  enum sexuality: { 
+    man: 1,
+    woman: 2,
+    other: 99
+  }
 end
