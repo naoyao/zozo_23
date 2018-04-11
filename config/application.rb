c96a11a458a26fe1e4e207ea1ef63b749034c2fb
Sakurai Shun <sakushun@SakurainoMacBook-Pro.local>
2018-04-11 10:03:56 +0900

    change-language-to-JP

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Zozo23
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # to auto load lib/ directory
+    config.i18n.default_locale = :ja
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
