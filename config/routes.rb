74baf5767b3577819e27fb5d3f8bb4df6c9f5a93
naoyao <na0418end.ne@gmail.com>
2018-04-18 17:17:44 +0900

    chenge_config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
 
-  get 'top/index'
-  get 'products/index' =>'products#index'
+  resources :products
+  resources :top, only: [:index]
 end
