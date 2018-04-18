b5d6570063853e8f0b08db898cda9117a0e2a7bf
naoyao <na0418end.ne@gmail.com>
2018-04-18 16:22:02 +0900

    Add_product_view#1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'top/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
-  # get 'product/product' 
+
+  get 'top/index'
+  get 'products/index' =>'products#index'
 end
