fb54031c059aaeaeed594ebdcb45c4aba8ed6017
naoyao <na0418end.ne@gmail.com>
2018-04-19 13:10:41 +0900

    change_conflict#2

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,13 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  
-  # 生成されるファイルを設定
-    config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.template_engine false
-    g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
-    g.fixture_replacement :factory_girl, dir: "spec/support/factories"
-  end
+
 end
