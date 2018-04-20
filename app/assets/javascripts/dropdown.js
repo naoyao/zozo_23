fcc45ec43d28645accf1a387569ff853808a9616
渥美佑佳 <imusta99ym@gmail.com>
2018-04-20 12:43:36 +0900

    Fix_category_Product_List
    カテゴリー選択時の商品一覧ページのビューについて軽微な修正を行っている。
    ・サイドのカテゴリー選択欄に対して、小カテゴリのメニューを表示させるjQueryの実装を試みている。
    ・他のブランチで作成された内容との影響を修正している。

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -8,3 +8,13 @@ $(function(){
 		}
 	});
 });
+
+$('.drop_cat').each(function(){
+  $(this).css("height",$(this).height()+"px");
+});
+$('.drop_cat').hide();
+$('.open_btn').click(function () {
+  $(this).next('.drop_cat').slideToggle('slow').siblings('.drop_cat').slideUp('slow');
+  $(this).siblings('.open_btn').removeClass('active')
+  $(this).toggleClass('active');
+});
